155d25c50b971b51d219f494615b32cf3d09e0d9
Hiroaki Date <d4te74@gmail.com>
2023-09-01 22:55:43 +0900

    install react-window

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,9 @@
-
+import { VariableSizeGrid, GridChildComponentProps } from "react-window";
 
 export default function Home() {
   return (
     <>
-      hoge
+      <VariableSizeGrid
     </>
   )
 }
